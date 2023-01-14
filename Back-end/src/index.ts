deb92b85c08f7615aaefbe41f6785d3649f23f88
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-01-14 11:55:07 -0300

    refactor: Resolvendo error de cors

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -3,12 +3,13 @@ import { AddressInfo } from "net";
 import express from "express";
 import { userRouter } from "./routes/userRouter";
 import { clientRouter } from "./routes/clientRouter";
-
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/user", userRouter);
 app.use("/client", clientRouter);
